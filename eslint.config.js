5fcfd5949f6f175739034fbe15c1e43247042d17
季斌 <jibin@jibindeMacBook-Pro.local>
2024-12-24 14:19:28 +0800

    feat: init

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -14,7 +15,7 @@ export default tseslint.config(
       ...tseslint.configs.recommended,
       prettierConfig,
     ],
-    files: ['**/*.{ts,tsx,js,jsx}'],
+    files: ['*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -23,6 +24,7 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       prettier: prettierPlugin,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -33,6 +35,27 @@ export default tseslint.config(
       // 自定义覆盖
       'react-hooks/exhaustive-deps': 'error',
       'prettier/prettier': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            [
+              '^vue', // vue放在首行
+              // 以字母(或数字或下划线)或“@”后面跟着字母开头的东西,通常为nodeModules引入
+              '^@?\\w',
+              '^@(/.*|$)', // 内部导入 "@/"
+              '^\\.\\.(?!/?$)', // 父级导入. 把 `..` 放在最后.
+              '^\\.\\./?$',
+              // 同级导入. 把同一个文件夹.放在最后
+              '^\\./(?=.*/)(?!/?$)',
+              '^\\.(?!/?$)',
+              '^\\./?$',
+              '^.+\\.?(css|less|scss)$', // 样式导入.
+              '^\\u0000', // 带有副作用导入，比如import 'a.css'这种.
+            ],
+          ],
+        },
+      ],
     },
   },
 );
